5d543924f6bfbde80137e513f6b8fed3bf1e7fb3
nj_evangelista73yahoo.com <nj_evangelista73@yahoo.com>
2026-04-19 23:01:57 +0800

    task1

---
+++ task/Task1.java
@@ -0,0 +1,15 @@
+package task;
+
+public class Task1 {
+  
+class KamustaMundo  {
+    public static void main(String[] args) {
+    String Mundo = new String("Kamusta Mundo!");
+    // System.out.print(" Kamusta Mundo! "); you can use this instead same output
+    System.out.println(Mundo);  
+    
+}}}
+
+
+
+  
