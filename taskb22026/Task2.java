aee5241e6524c42a4c328b2c771fcfb81c19fbb4
njcrux73 <nj_evangelista73@yahoo.com>
2026-05-06 15:25:56 +0000

    Task2

---
+++ taskb22026/Task2.java
@@ -0,0 +1,21 @@
+package taskb22026;
+
+public class Task2 {
+    public static void main(String[] args) {
+    
+        char letter = 'H';
+        byte zero = 3;
+        char letter2 = 'l';
+        char letter3 = 'l';
+        short one = 0;
+        int o = 0;
+        float f = 2.0f;
+        boolean isTrue = true;
+
+        String output = "H"+zero+letter2+letter3+one+ " " + "w"+o+"r"+"l"+"d" + " " +f+ " "+ true;
+        System.out.println(output);
+
+        
+
+    }    
+}
