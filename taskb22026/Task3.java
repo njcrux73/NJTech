b0fc56cee3e07c54c3ada819cbc02504acb5bb0f
njcrux73 <nj_evangelista73@yahoo.com>
2026-05-08 14:18:55 +0000

    Task3

---
+++ taskb22026/Task3.java
@@ -0,0 +1,19 @@
+package taskb22026; 
+
+public class Task3 {
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = a;
+        String c = "iran";
+        String d = new String("Wow");
+
+        boolean b1 = a==b;
+        boolean b2 = d.equals(b+"");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+
+    }
+}
